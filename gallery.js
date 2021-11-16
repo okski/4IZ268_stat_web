34d69c8f5322b783669349bb2258b42928859a4a
Jakub Hošek <hosj03@vse.cz>
2021-11-16 22:18:40 +0100

    Gallery view fix.

--- gallery.js
+++ gallery.js
@@ -21,17 +21,20 @@ function currentSlide(n) {
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("slide");
+    // document.style.display = "none";
     // var dots = document.getElementsByClassName("demo");
     // var captionText = document.getElementById("caption");
     if (n > slides.length) {slideIndex = 1}
+
     if (n < 1) {slideIndex = slides.length}
+
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     // for (i = 0; i < dots.length; i++) {
     //     dots[i].className = dots[i].className.replace(" active", "");
     // }
-    slides[slideIndex-1].style.display = "block";
+    slides[slideIndex-1].style.display = "flex";
     // dots[slideIndex-1].className += " active";
     // captionText.innerHTML = dots[slideIndex-1].alt;
 }
